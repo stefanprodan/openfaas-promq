6d106cb608629df96d3a5e0e79c6d49ba1386cb8
Stefan Prodan <stefan.prodan@gmail.com>
2017-12-03 14:53:38 +0200

    return JSON response

--- promq/handler.go
+++ promq/handler.go
@@ -6,5 +6,30 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	r, err := NewRequest(req)
+	if err != nil {
+		return fmt.Sprintf("Request parse error: %v", err)
+	}
+
+	start, end, step, err := r.GetQueryRange()
+	if err != nil {
+		return fmt.Sprintf("Query range parse error: %v", err)
+	}
+
+	c, err := NewClient(r.Server, "", "")
+	if err != nil {
+		return fmt.Sprintf("HTTP client error: %v", err)
+	}
+
+	response, err := c.QueryRange(r.Query, start, end, step)
+	if err != nil {
+		return fmt.Sprintf("Query error: %v", err)
+	}
+
+	json, err := responseJSON(response)
+	if err != nil {
+		return fmt.Sprintf("Query result parse error: %v", err)
+	}
+
+	return json
 }
